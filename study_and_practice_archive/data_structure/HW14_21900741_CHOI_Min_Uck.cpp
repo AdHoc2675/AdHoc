bbabe3259c7e82752f88528346837a92331a8622
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2022-05-30 23:32:00 +0900

    Add files via upload

---
+++ study_and_practice_archive/data_structure/HW14_21900741_CHOI_Min_Uck.cpp
@@ -0,0 +1,229 @@
+﻿// HW14_21900741_CHOI_Min_Uck.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class bst_node {
+public:
+    string birthday;
+    string name;
+    string p_num;
+    bst_node* left, * right;
+    bst_node();
+    bst_node(string s1, string s2, string s3);
+    void set_data(string s1, string s2, string s3);
+};
+
+bst_node::bst_node() {
+    birthday = "0000-00-00";
+    name = "None";
+    p_num = "000-0000-0000";
+}
+
+bst_node::bst_node(string s1, string s2, string s3) {
+    birthday = s1;
+    name = s2;
+    p_num = s3;
+}
+
+void bst_node::set_data(string s1, string s2, string s3) {
+    birthday = s1;
+    name = s2;
+    p_num = s3;
+}
+
+class bst_tree {
+    bst_node* root;
+    int csize;
+public:
+
+    bst_tree();
+    bst_node search(string tid);
+    void insert_node(bst_node t);
+    bool empty();
+    int size();
+    void show_inorder();
+};
+
+bst_tree::bst_tree() {
+    root = NULL;
+    csize = 0;
+}
+
+bst_node bst_tree::search(string tid) {
+    bst_node* p;
+    p = root;
+
+    if (root == NULL) {
+        bst_node tmp;
+        tmp.set_data("0000-00-00", "None", "000-0000-0000");
+        cout << "The key " << tid << " does not exist\n";
+        return tmp;
+    }
+
+    while (1) {
+        if (p->name == tid) {
+            return (*p);
+        }
+        if (p->name < tid) {
+            if (p->right = NULL) {
+                bst_node tmp;
+                tmp.set_data("0000-00-00", "None", "000-0000-0000");
+                cout << "The key " << tid << " does not exist\n";
+                return tmp;
+            }
+            else {
+                p = p->right;
+            }
+        }
+        else {
+            if (p->left == NULL) {
+                bst_node tmp;
+                tmp.set_data("0000-00-00", "None", "000-0000-0000");
+                cout << "The key " << tid << " does not exist\n";
+                return tmp;
+            }
+            else {
+                p = p->left;
+            }
+        }
+    }
+
+    return *p;
+}
+
+void bst_tree::insert_node(bst_node t) {
+    bst_node* p;
+    bst_node* tmp;
+
+    tmp = new bst_node;
+    *tmp = t;
+    tmp->left = NULL;
+    tmp->right = NULL;
+
+    if (root == NULL) {
+        root = tmp;
+        return;
+    }
+
+    p = root;
+
+    while (1) {
+        if (p->p_num == t.p_num) {
+            cout << "Insertion Failed : the phone number " << t.p_num << " already exists.\n";
+            return;
+        }
+        if (p->birthday < t.birthday) {
+            if (p->right == NULL) {
+                p->right = tmp;
+                csize++;
+                return;
+            }
+            else {
+                p = p->right;
+            }
+        }
+        else {
+            if (p->left == NULL) {
+                p->left = tmp;
+                csize++;
+                return;
+            }
+            else {
+                p = p->left;
+            }
+        }
+    }
+}
+
+void print_inorder(bst_node* p) {
+
+    if (p == NULL) {
+        return;
+    }
+
+    print_inorder(p->left);
+    cout << p->birthday << " : " << p->name << " : " << p->p_num << "\n";
+    print_inorder(p->right);
+}
+
+void bst_tree::show_inorder() {
+    print_inorder(root);
+}
+
+bool bst_tree::empty() {
+    if (root == NULL) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int bst_tree::size() {
+    return csize;
+}
+
+int main()
+{
+    bst_node temp;
+    bst_tree t1;
+
+    int order;
+    string input_birth;
+    string input_name;
+    string input_p_num;
+    string key;
+
+    while (1) {
+        cout << "What are you want to do?\n";
+        cout << "Please enter the number.\n";
+        cout << "1. Insert a New Node\n";
+        cout << "2. Search existing nodes with Name\n";
+        cout << "3. See Whole List\n"; //inorder 방식
+        cout << "4. End Program\n";
+
+        cin >> order;
+
+        if (order == 1) { //노드 입력, insert 기능
+            cout << "To insert a New Node, Please enter the Birthday, Name, and Phone Number\n";
+            cout << "Enter the Birthday in form of 0000-00-00\n";
+            cin >> input_birth;
+            cout << "Enter the Name\n";
+            cin >> input_name;
+            cout << "Enter the Phone Numbe in form of 000-0000-0000\n";
+            cin >> input_p_num;
+
+            temp.set_data(input_birth, input_name, input_p_num);
+            t1.insert_node(temp);
+        }
+        else if (order == 2) { //노드 조회, search 기능
+            cout << "Please enter the Name\n";
+
+            cin >> key;
+
+            temp = t1.search(key);
+
+            cout << "\n -- " << key << "'s record ---" << endl;
+            cout << " Birthday     : " << temp.birthday << endl;
+            cout << " Name         : " << temp.name << endl;
+            cout << " Phone Number : " << temp.p_num << endl;
+
+        }
+        else if (order == 3) { //inoder 방식으로 출력
+            cout << "\nNode List : inorder sequence \n";
+            t1.show_inorder();
+        }
+        else if (order == 4) {
+            cout << "Program Ended.\n";
+            break;
+        }
+        else {
+            cout << "Wrong Input. Please enter the number again.\n\n";
+        }
+    }
+    
+    return 0;
+}
---
+++ study_and_practice_archive/data_structure/HW15_21900741_CHOI_Min_Uck.cpp
@@ -0,0 +1,222 @@
+﻿// HW15_21900741_CHOI_Min_Uck.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+
+#include <iostream>
+using namespace std;
+
+class s_record {
+public:
+    string s_id;
+    string name;
+    double score;
+
+    s_record();
+    s_record(string s1, string s2, double n);
+};
+
+s_record::s_record() {
+    s_id = "None";
+    name = "None";
+    score = 0.0;
+}
+
+s_record::s_record(string s1, string s2, double n) {
+    s_id = s1;
+    name = s2;
+    score = n;
+}
+
+void insertion_sort(s_record a[], int n) {
+    for (int j = 1; j < n; j++) {
+        s_record temp = a[j];
+        int k = j - 1;
+
+        while ((k >= 0) && (a[k].s_id > temp.s_id)) {
+            a[k + 1] = a[k];
+            k--;
+        }
+
+        a[k + 1] = temp;
+    }
+}
+
+void bubble_sort(s_record a[], int n) {
+    s_record temp;
+
+    for (int j = n - 1; j >= 1; j--) {
+        for (int k = 0; k < j; k++) {
+            if (a[k].s_id > a[k + 1].s_id) {
+                temp = a[k];
+                a[k] = a[k + 1];
+                a[k + 1] = temp;
+            }
+        }
+    }
+}
+
+void selection_sort(s_record a[], int n) {
+    s_record temp;
+
+    for (int j = 0; j < n; j++) {
+        int min_i = j;
+        for (int k = j + 1; k < n; k++) {
+            if (a[k].s_id < a[min_i].s_id) {
+                min_i = k;
+            }
+        }
+        temp = a[j];
+        a[j] = a[min_i];
+        a[min_i] = temp;
+    }
+}
+
+void adjust(s_record a[], int troot, int size){
+    string id;
+    int child;
+    s_record temp;
+
+    temp = a[troot];
+    id = a[troot].s_id;
+    child = 2 * troot;
+
+    while (child <= size) {
+        if ((child < size) && (a[child].s_id < a[child + 1].s_id)) {
+            child++;
+        }
+        if (id > a[child].s_id) {
+            break;
+        }
+        else {
+            a[child / 2] = a[child];
+            child = child * 2;
+        }
+    }
+    a[child / 2] = temp;
+}
+
+void heap_sort(s_record a[], int n) {
+    int i; s_record b[13], temp;
+    for (i = 0; i < n; i++)                      // 1부터 n번째까지로 위치 조정
+        b[i + 1] = a[i];
+
+    for (i = n / 2; i > 0; i--)                   // 주어진 입력에 대하여 heap 구성
+        adjust(b, i, n);
+
+    for (i = n - 1; i > 0; i--) {
+        temp = b[1];
+        b[1] = b[i + 1];
+        b[i + 1] = temp; adjust(b, 1, i);
+    }
+    
+    for (int i = 0; i < 12; i++) {
+        a[i] = b[i + 1];
+    }
+    
+    // b에서 a로 copy하는 과정 추가}
+}
+void merge(s_record a[], s_record b[], int n1, int n2, int n3, int n4){
+    int i, j, k, t;
+    i = n1;
+    j = n3;
+    k = n1;
+
+    while ((i <= n2) && (j <= n4)){
+        if(a[i].s_id <= a[j].s_id)
+            b[k++] = a[i++];
+        else
+            b[k++] = a[j++];
+    }if (i > n2)
+        for (t = j; t <= n4; t++)
+            b[t] = a[t];
+    else
+        for(t = i; t <= n2; t++)
+            b[k + t - i] = a[t];
+}
+
+void merge_pass(s_record a[], s_record b[], int n, int s) {
+    int i, j;
+    for (i = 0; i < (n - 2 * s + 1); i = i + 2 * s) {
+        merge(a, b, i, i + s - 1, i + s, i + 2 * s - 1);
+    }
+    if (i + s <= n) {
+        merge(a, b, i, i + s - 1, i + s, n);
+    }
+    else {
+        for (j = i; j <= n; j++) {
+            b[j] = a[j];
+        }
+    }
+}
+
+void merge_sort(s_record a[], int n) {
+    int s = 1;
+    s_record b[13];
+
+    while (s < n) {
+        merge_pass(a, b, n - 1, s);
+        s = s * 2;
+        merge_pass(b, a, n - 1, s);
+        s = s * 2;
+    }
+}
+
+
+void quick_sort(s_record a[], int left, int right){
+    s_record pivot, temp;
+    int i, j;
+
+    if (left >= right)
+        return;
+
+    i = left;
+    j = right + 1;
+    pivot = a[left];
+    
+    do {
+        do {
+            i++;
+        } while ((i <= right) && (a[i].s_id < pivot.s_id));
+        do {
+            j--;
+        } while (a[j].s_id > pivot.s_id);
+        if (i < j)
+            swap(a[i], a[j]);
+    } while (i < j);
+
+    swap(a[left], a[j]);
+    quick_sort(a, left, j - 1);
+    quick_sort(a, j + 1, right);
+}
+
+
+void show_thelist(s_record a[], int n) {
+    for (int i = 0 ; i < n ; i++) {
+        cout << a[i].s_id << " : ";
+        cout << a[i].name << " : ";
+        cout << a[i].score << "\n";
+    }
+}
+
+int main()
+{
+    s_record s_list[12] =
+    { {"21900013", "Kim", 6.5}, {"21900136", "Lee", 8.8}, {"21900333", "Park", 9.2},
+    { "21800442", "Choi", 7.1}, {"21900375", "Ryu", 5.4}, {"21700248", "Cho", 6.3},
+    { "21700302", "Jung", 8.3}, {"21800255", "Han", 6.9}, {"21800369", "Kang", 6.3},
+    { "21900401", "Yang", 9.1}, {"21800123", "Moon", 8.1}, {"21700678", "Seo", 7.9}};
+    int    n = 12;
+
+    //insertion_sort(s_list, n); // 해결
+    // bubble_sort(s_list, n); //해결
+    // selection_sort(s_list, n); //해결
+
+    // heap_sort(s_list, n); //해결
+     merge_sort(s_list, n); //해결
+    //quick_sort(s_list, 0, n-1); //해결
+   
+    cout << "< The result of the sorting >" << endl;
+    show_thelist(s_list, n);
+
+    return 0;
+
+}
